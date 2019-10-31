e9682db2f189132cdc7174d768c1a4585c5e2aa4
Andrey Stolpner <andrey.stolpner@neotech.by>
2019-10-31 22:08:42 +0300

    added en point GET tasks by user

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -12,14 +12,26 @@ router.post('/', async ctx => {
 
     const collection = ctx.db.collection('tasks');
     const { ops: [ task ] } = await collection.insertOne({
+      userId: ctx.state.userId,
       name,
       createdAt: currentTime,
       changedAt: currentTime,
-      todo: [],
       status: false,
     });
 
+    delete task.userId;
     ctx.body = task;
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+});
+
+router.get('/', async ctx => {
+  try {
+    const collection = ctx.db.collection('tasks');
+    ctx.body = await collection.find({
+      userId: ctx.state.userId,
+    }, { projection: { userId: 0 } }).toArray();
   } catch (err) {
     ctx.throw(500, err);
   }
