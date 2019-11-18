8083b01ec5ecc6b0d501e7397d27153974bda186
Andrey Stolpner <andreysx@tut.by>
2019-11-18 19:59:08 +0300

    added edit user by admin endpoint

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -36,4 +36,48 @@ router.get('/tasks/user/:id', async ctx => {
   }
 });
 
+router.put('/user/:id/edit', async ctx => {
+  try {
+    const {
+      firstName,
+      lastName,
+      position,
+      role,
+    } = ctx.request.body;
+
+    const { id } = ctx.params;
+
+    const dataForEdit = {
+      firstName,
+      lastName,
+      position,
+      role,
+    };
+
+    Object.keys(dataForEdit).forEach(key => {
+      if (!dataForEdit[key]) {
+        delete dataForEdit[key];
+      }
+    });
+    
+    const collection = ctx.db.collection('users');
+    const { value } = await collection.findOneAndUpdate(
+      {
+        _id: ctx.ObjectID(id),
+      },
+      {
+        $set: dataForEdit,
+      },
+      {
+        returnOriginal: false,
+        projection: { _id: 0, password: 0 }
+      },
+    );
+
+    ctx.body = value;
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+});
+
 module.exports = router;
